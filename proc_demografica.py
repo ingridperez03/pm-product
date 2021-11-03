4887ee2fe4ae880d216a33f6e59bfa34c7a31e55
Ingrid Pérez <ingrid.perez03@estudiant.upf.edu>
2021-11-03 21:09:39 +0100

    Update indicador CP

--- proc_demografica.py
+++ proc_demografica.py
@@ -8,13 +8,14 @@ from indicador import Indicador
 def initCP():
     data = pd.DataFrame()
     for any in range(2000, 2021):
-        nomArxiu = "CP_" + str(any)
-        dataAny = pd.read_csv(nomArxiu)
+        nomArxiu = "CP_" + str(any) + ".csv"
+        path = "dades\\demografia\\creixement_poblacio\\"
+        dataAny = pd.read_csv(path + nomArxiu, index_col=None)
         # Preprocessing necessari 
         dataAny["Any"] = any
 
-        # Tota total 
-        dataAny['Total'] = dataAny.sum(axis = 1)
+        # Total total 
+        dataAny['Total'] = dataAny['Total. De 0 a 14 anys'] + dataAny['Total. De 15 a 64 anys'] + dataAny['Total. 65 anys o més']
 
         # Total per sexes 
         dataAny['Homes'] = dataAny['Homes. De 0 a 14 anys'] + dataAny['Homes. De 15 a 64 anys'] + dataAny['Homes. 65 anys o més']
@@ -25,10 +26,11 @@ def initCP():
         dataAny['15-64 Anys'] = dataAny['Homes. De 15 a 64 anys'] + dataAny['Dones. De 15 a 64 anys']
         dataAny['64 o més'] = dataAny['Homes. 65 anys o més'] + dataAny['Dones. 65 anys o més']
 
-        dataAny.drop(['Homes. De 0 a 14 anys','Dones. De 0 a 14 anys', 'Homes. De 15 a 64 anys', 
-                      'Dones. De 15 a 64 anys', 'Homes. 65 anys o més', 'Dones. 65 anys o més'], axis = 1)
+        dataAny.drop(['Homes. De 0 a 14 anys', 'Dones. De 0 a 14 anys', 'Homes. De 15 a 64 anys', 
+                      'Dones. De 15 a 64 anys', 'Homes. 65 anys o més', 'Dones. 65 anys o més',
+                      'Total. De 0 a 14 anys', 'Total. De 15 a 64 anys', 'Total. 65 anys o més'], axis = 1, inplace=True)
 
-        data.append(dataAny)
+        data = data.append(dataAny)
     
     data.info()
     indicador = Indicador(data, range(2000, 2021), "municipi", "unitats")
