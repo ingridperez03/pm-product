772267fd13a9e436f635cdd4d0085b4c83a8ef48
Ingrid Pérez <ingrid.perez03@estudiant.upf.edu>
2021-11-03 20:16:39 +0100

    Afegir dades

--- proc_demografica.py
+++ proc_demografica.py
@@ -10,8 +10,24 @@ def initCP():
     for any in range(2000, 2021):
         nomArxiu = "CP_" + str(any)
         dataAny = pd.read_csv(nomArxiu)
-        # Preprocessing necessari
+        # Preprocessing necessari 
         dataAny["Any"] = any
+
+        # Tota total 
+        dataAny['Total'] = dataAny.sum(axis = 1)
+
+        # Total per sexes 
+        dataAny['Homes'] = dataAny['Homes. De 0 a 14 anys'] + dataAny['Homes. De 15 a 64 anys'] + dataAny['Homes. 65 anys o més']
+        dataAny['Dones'] = dataAny['Dones. De 0 a 14 anys'] + dataAny['Dones. De 15 a 64 anys'] + dataAny['Dones. 65 anys o més']
+
+        # Total per grups d'edat
+        dataAny['0-14 Anys'] = dataAny['Homes. De 0 a 14 anys'] + dataAny['Dones. De 0 a 14 anys']
+        dataAny['15-64 Anys'] = dataAny['Homes. De 15 a 64 anys'] + dataAny['Dones. De 15 a 64 anys']
+        dataAny['64 o més'] = dataAny['Homes. 65 anys o més'] + dataAny['Dones. 65 anys o més']
+
+        dataAny.drop(['Homes. De 0 a 14 anys','Dones. De 0 a 14 anys', 'Homes. De 15 a 64 anys', 
+                    'Dones. De 15 a 64 anys', 'Homes. 65 anys o més', 'Dones. 65 anys o més'], axis = 1)
+
         data.append(dataAny)
     
     data.info()
