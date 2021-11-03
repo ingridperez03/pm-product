30b37c8424fac9c08128446f574cc39cad5af5d4
davidgrao2000 <david.grao01@estudiant.upf.edu>
2021-11-03 22:20:52 +0100

    urbanistica_solar

--- proc_urbanistica.py
+++ proc_urbanistica.py
@@ -19,11 +19,30 @@ def initSe():
         dataAny = dataAny[['Any', 'NomMun', 'Codi_ine_6_txt', '12_A2_SUC']]
         dataAny = dataAny.rename(columns={'NomMun':'Municipi', 'Codi_ine_6_txt':'Codi', '12_A2_SUC':'Serveis'})
         data = data.append(dataAny, ignore_index = True)
-    print(data)
 
     indicador = Indicador(data, range(2012, 2021), "municipi", "Ha")
 
     return indicador
+
+
+'''
+    Netejar dades referents als solars (So)
+'''
+def initSo():
+    data = pd.DataFrame()
+    path = os.path.join(data_path,'solar')
+
+    for any in range(2012, 2021):
+        nomArxiu = "dades_mapa_" + str(any) + ".csv"
+        dataAny = pd.read_csv(os.path.join(path,nomArxiu))
+        dataAny = dataAny[['Any', 'NomMun', 'Codi_ine_6_txt', '15_Qual_SUC_SISTEMES']]
+        dataAny = dataAny.rename(columns={'NomMun':'Municipi', 'Codi_ine_6_txt':'Codi', '15_Qual_SUC_SISTEMES':'Solar'})
+        data = data.append(dataAny, ignore_index = True)
+
+    indicador = Indicador(data, range(2012, 2021), "municipi", "Ha")
+
+    return indicador
+
 
 
 def initUrbanistica():
@@ -31,5 +50,8 @@ def initUrbanistica():
 
     serveis = initSe()
     dimensio.afegirIndicador("Serveis", serveis)
+
+    solar = initSo()
+    dimensio.afegirIndicador("Solar", solar)
 
     return dimensio
