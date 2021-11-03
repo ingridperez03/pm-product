5889b7d2ba9820f241c70c9a6ddf722c52732a7c
Ingrid Pérez <ingrid.perez03@estudiant.upf.edu>
2021-11-03 20:47:13 +0100

    Add missing dimensio

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ from aplicacio import Aplicacio
 from indicador import Indicador
 from dimensio import Dimensio
 from proc_demografica import initDemografica
+# from proc_urbanistica import initUrbanistica
+# from proc_economica import initEconomica
+# from proc_residencial import initResidencial
 
 def initApp(App):
     App = Aplicacio()
     dimensioDemografica = initDemografica()
-    App.afegirDimensio("Demogràfica", dimensioDemografica)
+    App.afegirDimensio("Demografica", dimensioDemografica)
+    # dimensioUrbanistica = initDemografica()
+    # App.afegirDimensio("Urbanistica", dimensioUrbanistica)
+    # dimensioEconomica = initDemografica()
+    # App.afegirDimensio("Economica", dimensioEconomica)
+    # dimensioResidencial = initDemografica()
+    # App.afegirDimensio("Residencial", dimensioResidencial)
 
 if __name__ == "__main__":
     initApp()
