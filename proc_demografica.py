ad98aa899636ded2f95c23e1c25bcf7686be0149
clarareolid01 <clara.reolid01@estudiant.upf.edu>
2021-11-04 11:33:50 +0100

    Indicador fertilitat

--- proc_demografica.py
+++ proc_demografica.py
@@ -1,16 +1,17 @@
 import pandas as pd
+import os
 from dimensio import Dimensio
 from indicador import Indicador
 
 '''
     Netejar dades referents al creixement població (CP)
 '''
+data_path = os.path.join('dades', 'demografia')
 def initCP():
     data = pd.DataFrame()
     for any in range(2000, 2021):
         nomArxiu = "CP_" + str(any) + ".csv"
-        path = "dades\\demografia\\creixement_poblacio\\"
-        dataAny = pd.read_csv(path + nomArxiu, index_col=None)
+        dataAny = pd.read_csv(os.path.join(data_path, 'creixement_poblacio', nomArxiu), index_col=None)
         # Preprocessing necessari 
         dataAny["Any"] = any
 
@@ -29,9 +30,50 @@ def initCP():
         dataAny.drop(['Homes. De 0 a 14 anys', 'Dones. De 0 a 14 anys', 'Homes. De 15 a 64 anys', 
                       'Dones. De 15 a 64 anys', 'Homes. 65 anys o més', 'Dones. 65 anys o més',
                       'Total. De 0 a 14 anys', 'Total. De 15 a 64 anys', 'Total. 65 anys o més'], axis = 1, inplace=True)
-
+        
         data = data.append(dataAny)
     
+    data.info()
+    indicador = Indicador(data, range(2000, 2021), "municipi", "unitats")
+    return indicador
+
+'''
+    Netejar dades referents a la fertilitat (NPD)
+'''
+def initF():
+    data = pd.DataFrame()
+    for any in [1991, 2011]:
+        nomArxiu = "NPD_" + str(any) + ".csv"
+        dataAny = pd.read_csv(os.path.join(data_path, 'fertilitat', nomArxiu), encoding='latin-1', index_col=None)
+        
+        # Preprocessing necessari 
+        dataAny["Any"] = any
+
+        # Editar file columna 0 te un espai davant 
+        dataAny.drop([' 0', '1', '2', '3', '4 i més', 'Total Dones', 'Total Fills'], axis = 1, inplace=True)
+        
+        data = data.append(dataAny)
+
+    data.info()
+    indicador = Indicador(data, [1991, 2011], "municipi", "unitats")
+    return indicador
+
+'''
+    Netejar dades referents a la migracions (M)
+'''
+def initM():
+    data = pd.DataFrame()
+    for any in range(2000, 2021):
+        nomArxiu = "NPD_" + str(any) + ".csv"
+        dataAny = pd.read_csv(os.path.join(data_path, 'migracions', nomArxiu), index_col=None)
+        
+        # Preprocessing necessari 
+        dataAny["Any"] = any
+
+        # Suma migracions  
+        
+        data = data.append(dataAny)
+
     data.info()
     indicador = Indicador(data, range(2000, 2021), "municipi", "unitats")
     return indicador
@@ -42,5 +84,13 @@ def initDemografica():
     # Creixement Població
     creixement = initCP()
     dimensio.afegirIndicador("Creixement Poblacio", creixement)
+
+    # Fertilitat 
+    fertilitat = initF()
+    dimensio.afegirIndicador("Fertilitat", fertilitat)
+
+    # Migracions
+    migracions = initM()
+    dimensio.afegirIndicador("Migracions", migracions)
 
     return dimensio
