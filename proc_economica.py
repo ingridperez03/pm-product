b18e607e210776cc42727b72c600cd4dbfb674f0
davidlegarre <77801298+davidlegarre@users.noreply.github.com>
2021-11-05 15:01:43 +0100

    Afegit evolució atur

--- proc_economica.py
+++ proc_economica.py
@@ -6,31 +6,62 @@ import os
     Netejar dades referents  al atur per any
 '''
 
-data_path = os.path.join('dades','economiques')
+data_path = os.path.join('dades', 'economiques')
+
 
 def initAtur():
-    data = pd.read_csv(os.path.join(data_path,'Atur','aturats_catalunya_12-20.csv'))
-    
-    indicador = Indicador(data, list(range(2012,2020)), "municipi", "unitats")
+    data = pd.read_csv(os.path.join(data_path,
+                                    'Atur',
+                                    'aturats_catalunya_12-20.csv'))
+
+    indicador = Indicador(data, list(range(2012, 2021)), "municipi", "unitats")
+
     return indicador
-        
+
+
+def initEvAtur():
+    data = pd.read_csv(os.path.join(data_path,
+                                    'EvolucioAtur',
+                                    'evolucio_atur_12-20.csv'))
+
+    indicador = Indicador(data, list(range(2012, 2021)), "municipi", "unitats")
+
+    return indicador
+
+
 def initPobAct():
-    data = pd.read_csv(os.path.join(data_path,'PoblacioActiva','poblacio_activa_2011.csv'))
+    data = pd.read_csv(os.path.join(data_path,
+                                    'PoblacioActiva',
+                                    'poblacio_activa_2011.csv'))
 
     indicador = Indicador(data, list(2011), "municipi", "unitats")
+
     return indicador
+
 
 def initRFDB():
-    data = pd.read_csv(os.path.join(data_path,'RendaFamiliar','RFDB_evolució_2010_2018.csv')
+    data = pd.read_csv(os.path.join(data_path,
+                                    'RendaFamiliar',
+                                    'RFDB_evolució_2010_2018.csv'))
 
-        indicador = Indicador(data, list(range(2010,2018), "municipi amb més de mil habitants","unitats"
+    indicador = Indicador(data,
+                          list(range(2010, 2019)),
+                          "municipi amb més de mil habitants",
+                          "milers")
+
     return indicador
+
+
 def initEconomica():
     dimensio = Dimensio()
 
     # Mitjana aturats per any i municipi
     aturats = initAtur()
     dimensio.afegirIndicador("Aturats per any", aturats)
+
+    # Afegir evolució de l'atur
+    ev_atur = initEvAtur()
+    dimensio.afegirIndicador("Evolució de l'atur", ev_atur)
 
     # Afegir població activa, del estudi del 2011
     pob_activa = initPobAct()
