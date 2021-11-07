c234dcb98762bacc12633c7868f26db47859fa11
Ingrid Pérez <ingrid.perez03@estudiant.upf.edu>
2021-11-07 11:12:20 +0100

    Actualitzar dimensió urbanística

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def initApp(App):
+from aplicacio import Aplicacio
+from proc_urbanistica import initUrbanistica
+
+def initApp():
     App = Aplicacio()
-    dimensioDemografica = initDemografica()
-    App.afegirDimensio("Demogràfica", dimensioDemografica)
+    dimensioUrbanistica = initUrbanistica()
+    App.afegirDimensio("Urbanistica", dimensioUrbanistica)
 
 if __name__ == "__main__":
     initApp()
--- proc_urbanistica.py
+++ proc_urbanistica.py
@@ -3,8 +3,8 @@ import pandas as pd
 from dimensio import Dimensio
 from indicador import Indicador
 
-
-data_path = os.path.join('dades','urbanistica')
+# Path localització dades urbanistiques
+data_path = os.path.join('dades', 'urbanistica')
 
 '''
     Netejar dades referents als serveis (Se)
@@ -15,13 +15,13 @@ def initSe():
 
     for any in range(2012, 2021):
         nomArxiu = "dades_mapa_" + str(any) + ".csv"
-        dataAny = pd.read_csv(os.path.join(path,nomArxiu))
+        dataAny = pd.read_csv(os.path.join(path, nomArxiu))
         dataAny = dataAny[['Any', 'NomMun', 'Codi_ine_6_txt', '12_A2_SUC']]
-        dataAny = dataAny.rename(columns={'NomMun':'Municipi', 'Codi_ine_6_txt':'Codi', '12_A2_SUC':'Serveis'})
+        dataAny.rename(columns={'NomMun':'Literal', 'Codi_ine_6_txt':'Codi', '12_A2_SUC':'Serveis'}, inplace=True)
         data = data.append(dataAny, ignore_index = True)
 
+    data.info()
     indicador = Indicador(data, range(2012, 2021), "municipi", "Ha")
-
     return indicador
 
 
@@ -30,17 +30,17 @@ def initSe():
 '''
 def initSo():
     data = pd.DataFrame()
-    path = os.path.join(data_path,'solar')
+    path = os.path.join(data_path, 'solar')
 
     for any in range(2012, 2021):
         nomArxiu = "dades_mapa_" + str(any) + ".csv"
-        dataAny = pd.read_csv(os.path.join(path,nomArxiu))
+        dataAny = pd.read_csv(os.path.join(path, nomArxiu))
         dataAny = dataAny[['Any', 'NomMun', 'Codi_ine_6_txt', '15_Qual_SUC_SISTEMES']]
-        dataAny = dataAny.rename(columns={'NomMun':'Municipi', 'Codi_ine_6_txt':'Codi', '15_Qual_SUC_SISTEMES':'Solar'})
+        dataAny.rename(columns={'NomMun':'Literal', 'Codi_ine_6_txt':'Codi', '15_Qual_SUC_SISTEMES':'Solar'}, inplace=True)
         data = data.append(dataAny, ignore_index = True)
 
+    data.info()
     indicador = Indicador(data, range(2012, 2021), "municipi", "Ha")
-
     return indicador
 
 
@@ -49,17 +49,17 @@ def initSo():
 '''
 def initZV():
     data = pd.DataFrame()
-    path = os.path.join(data_path,'zones_verdes')
+    path = os.path.join(data_path, 'zones_verdes')
 
     for any in range(2012, 2021):
         nomArxiu = "dades_mapa_" + str(any) + ".csv"
-        dataAny = pd.read_csv(os.path.join(path,nomArxiu))
+        dataAny = pd.read_csv(os.path.join(path, nomArxiu))
         dataAny = dataAny[['Any', 'NomMun', 'Codi_ine_6_txt', '15_SV_SUC']]
-        dataAny = dataAny.rename(columns={'NomMun':'Municipi', 'Codi_ine_6_txt':'Codi', '15_SV_SUC':'Zones verdes'})
+        dataAny.rename(columns={'NomMun':'Literal', 'Codi_ine_6_txt':'Codi', '15_SV_SUC':'Zones verdes'}, inplace=True)
         data = data.append(dataAny, ignore_index = True)
 
+    data.info()
     indicador = Indicador(data, range(2012, 2021), "municipi", "Ha")
-
     return indicador
 
 
@@ -68,17 +68,17 @@ def initZV():
 '''
 def initE():
     data = pd.DataFrame()
-    path = os.path.join(data_path,'equipaments')
+    path = os.path.join(data_path, 'equipaments')
 
     for any in range(2012, 2021):
         nomArxiu = "dades_mapa_" + str(any) + ".csv"
-        dataAny = pd.read_csv(os.path.join(path,nomArxiu))
+        dataAny = pd.read_csv(os.path.join(path, nomArxiu))
         dataAny = dataAny[['Any', 'NomMun', 'Codi_ine_6_txt', '20_Equip_habt']]
-        dataAny = dataAny.rename(columns={'NomMun':'Municipi', 'Codi_ine_6_txt':'Codi', '20_Equip_habt':'Equipament'})
+        dataAny.rename(columns={'NomMun':'Literal', 'Codi_ine_6_txt':'Codi', '20_Equip_habt':'Equipament'}, inplace=True)
         data = data.append(dataAny, ignore_index = True)
  
+    data.info()
     indicador = Indicador(data, range(2012, 2021), "municipi", "m2 / habitant")
-
     return indicador
 
 
@@ -87,20 +87,20 @@ def initE():
 '''
 def initCon():
     data = pd.DataFrame()
-    path = os.path.join(data_path,'connectivitat')
+    path = os.path.join(data_path, 'connectivitat')
 
     for any in range(2012, 2021):
         nomArxiu = "dades_mapa_" + str(any) + ".csv"
-        dataAny = pd.read_csv(os.path.join(path,nomArxiu))
+        dataAny = pd.read_csv(os.path.join(path, nomArxiu))
         dataAny = dataAny[['Any', 'NomMun', 'Codi_ine_6_txt', '15_SF_SUC', '15_SX1_SUC', '15_SX2_SUC']]
-        dataAny = dataAny.rename(columns={'NomMun':'Municipi', 'Codi_ine_6_txt':'Codi', '15_SF_SUC':'Ferroviari', '15_SX1_SUC':'Camins Principals', '15_SX2_SUC':'Camins Secundaris'})
+        dataAny.rename(columns={'NomMun':'Literal', 'Codi_ine_6_txt':'Codi', '15_SF_SUC':'Ferroviari', '15_SX1_SUC':'Camins Principals', '15_SX2_SUC':'Camins Secundaris'}, inplace=True)
         data = data.append(dataAny, ignore_index = True)
 
-    data["Score"] = data[["Ferroviari", "Camins Principals", "Camins Secundaris"]].sum(axis=1)
+    data["Connectivitat"] = data[["Ferroviari", "Camins Principals", "Camins Secundaris"]].sum(axis=1)
     data.drop(["Ferroviari", "Camins Principals", "Camins Secundaris"], axis=1, inplace=True)
 
+    data.info()
     indicador = Indicador(data, range(2012, 2021), "municipi", "Ha")
-
     return indicador
 
 
@@ -109,19 +109,18 @@ def initCon():
 '''
 def initCom():
     data = pd.DataFrame()
-    path = os.path.join(data_path,'comerç')
+    path = os.path.join(data_path, 'comerç')
 
     for any in range(2017, 2020):
         nomArxiu = "comerç_" + str(any) + ".csv"
-        dataAny = pd.read_csv(os.path.join(path,nomArxiu))
+        dataAny = pd.read_csv(os.path.join(path, nomArxiu))
         dataAny = dataAny[['Any', 'Municipi', 'Codi_ine_6_txt', 'Densitat comercial (m2 / 1.000 hab.)']]
-        dataAny = dataAny.rename(columns={'NomMun':'Municipi', 'Codi_ine_6_txt':'Codi', 'Densitat comercial (m2 / 1.000 hab.)':'Densitat comercial'})
+        dataAny.rename(columns={'NomMun':'Literal', 'Codi_ine_6_txt':'Codi', 'Densitat comercial (m2 / 1.000 hab.)':'Densitat comercial'}, inplace=True)
         data = data.append(dataAny, ignore_index = True)
 
+    data.info()
     indicador = Indicador(data, range(2017, 2020), "municipi", "m2 / 1.000 habitants")
-
     return indicador
-
 
 
 '''
@@ -132,41 +131,47 @@ def initR():
     path = os.path.join(data_path,'riscos')
 
     nomArxiu = "riscos.csv"
-    dataAny = pd.read_csv(os.path.join(path,nomArxiu))
+    dataAny = pd.read_csv(os.path.join(path, nomArxiu))
     dataAny = dataAny[['municipiNom', 'ine6', 'nivell']]
-    dataAny = dataAny.rename(columns={'municipiNom':'Municipi', 'ine6':'Codi', 'nivell':'Nivell'})
+    dataAny = dataAny.rename(columns={'municipiNom':'Literal', 'ine6':'Codi', 'nivell':'Nivell'})
     data = data.append(dataAny, ignore_index = True)
 
-    group = data.groupby(['Municipi', 'Codi', 'Nivell'])
+    group = data.groupby(['Literal', 'Codi'])
     data = group.size().reset_index(name='Riscos')
 
+    data.info()
     indicador = Indicador(data, 2021, "municipi", "unitats")
-
     return indicador
-
 
 
 def initUrbanistica():
     dimensio = Dimensio()
 
+    # Serveis
     serveis = initSe()
     dimensio.afegirIndicador("Serveis", serveis)
 
+    # Solar
     solar = initSo()
     dimensio.afegirIndicador("Solar", solar)
 
+    # Zones verdes
     zones_verdes = initZV()
     dimensio.afegirIndicador("Zones verdes", zones_verdes)
 
+    # Equipaments
     equipament = initE()
     dimensio.afegirIndicador("Equipament", equipament)
 
+    # Connectivitat
     connectivitat = initCon()
     dimensio.afegirIndicador("Connectivitat", connectivitat)
 
+    # Comerç
     comerç = initCom()
     dimensio.afegirIndicador("Comerç", comerç)
 
+    # Riscos
     riscos = initR()
     dimensio.afegirIndicador("Riscos", riscos)
 
